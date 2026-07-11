b8ebf56bd3948932d2d44db81f587d8dae42638b
happymusicapp <happymusicapp@gmail.com>
2026-07-11 11:57:35 -0300

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -670,6 +670,7 @@ const UI = (() => {
     el.iconPlay.classList.toggle('hidden', playing);
     el.iconPause.classList.toggle('hidden', !playing);
     el.btnPlayPause.classList.toggle('is-playing', playing);
+    el.seekBar.classList.toggle('is-playing', playing);
   }
 
   function updateProgress(current, duration) {
