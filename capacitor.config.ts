d76f7468a3a042c8bcb4d27fd3b287bc7eba1bd8
happymusicapp <happymusicapp@gmail.com>
2026-07-16 07:21:23 -0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.happymusic.app',
-  appName: 'HappyMusic',
-  webDir: 'www'
-};
-
-export default config;
